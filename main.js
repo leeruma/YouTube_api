a01fc098ac5404105addffdd56b6e8fadcaba345
leeruma <achieve4525@naver.com>
2023-09-22 15:12:34 +0900

    디자인 요소 추가

--- main.js
+++ main.js
@@ -4,18 +4,14 @@ const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 const pid = 'PLnBZ3Abl_z0NvoNbCbU2WV5-rkmzQsnwa';
 const num = 5;
 const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-const tit_len = 50;
+const tit_len = 30;
 const desc_len = 100;
 
-// 일정 글자수 이상일때 글자짜르고 말줄임표 붙이기
-// 문자열.substr(시작위치, 자를 글자수)
-
-// beef-lettuce-tomato --> Beef Lettuce Tomato
 let text = 'beef-lettuce-tomato';
 text = text
-	.split('-') // 기존 문자열에서 -을 기준으로 배열로 분리
-	.map((el) => el.charAt(0).toUpperCase() + el.slice(1)) // 분리된 문자값을 반복돌면서 첫 번째 글자만 대문자 변경 + 첫 번째를 제외한 나머지 문자 이어 붙임(각 단어의 첫글자만 대문자로 변경돼서 배열로 반환)
-	.join(' '); // 첫글자만 대문자로 변경된 단어들을 다시 빈칸으로 이어붙이기
+	.split('-')
+	.map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+	.join(' ');
 console.log(text);
 
 fetch(resultURL)
@@ -28,7 +24,6 @@ fetch(resultURL)
 			let desc = data.snippet.description;
 			desc.length > desc_len ? (desc = desc.substr(0, desc_len) + '...') : desc;
 
-			// 날자값 가공
 			let date = data.snippet.publishedAt.split('T')[0];
 			date = date.split('-').join('.');
 
